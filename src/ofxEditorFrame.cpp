843e700ecac352aade765bcbffffce15241bcb38
nariakiiwatani <nariakiiwatani@annolab.com>
2022-02-18 20:23:09 +0900

    first import

---
+++ src/ofxEditorFrame.cpp
@@ -0,0 +1,150 @@
+#include "ofxEditorFrame.h"
+#include "ofGraphics.h"
+#include "ofAppRunner.h"
+#include "ofEventUtils.h"
+
+ofxEditorFrame::~ofxEditorFrame()
+{
+	disableMouseInteraction();
+}
+
+void ofxEditorFrame::setup()
+{
+	enableMouseInteraction();
+}
+
+void ofxEditorFrame::update()
+{
+	if(has_unprocessed_mouse_event_) {
+		has_unprocessed_mouse_event_ = false;
+		ofNotifyEvent(on_mouse_event_, makeArg(), this);
+		is_mouse_event_sent_once_ = true;
+		mouse_prev_ = mouse_;
+	}
+}
+
+void ofxEditorFrame::enableMouseInteraction()
+{
+	if(!is_enabled_mouse_interaction_) {
+		auto &ev = ofEvents();
+		ofAddListener(ev.mousePressed, this, &ofxEditorFrame::onMousePressed);
+		ofAddListener(ev.mouseReleased, this, &ofxEditorFrame::onMouseReleased);
+		ofAddListener(ev.mouseMoved, this, &ofxEditorFrame::onMouseMoved);
+		ofAddListener(ev.mouseDragged, this, &ofxEditorFrame::onMouseDragged);
+		ofAddListener(ev.mouseScrolled, this, &ofxEditorFrame::onMouseScrolled);
+
+		is_enabled_mouse_interaction_ = true;
+	}
+}
+void ofxEditorFrame::disableMouseInteraction()
+{
+	if(is_enabled_mouse_interaction_) {
+		auto &ev = ofEvents();
+		ofRemoveListener(ev.mousePressed, this, &ofxEditorFrame::onMousePressed);
+		ofRemoveListener(ev.mouseReleased, this, &ofxEditorFrame::onMouseReleased);
+		ofRemoveListener(ev.mouseMoved, this, &ofxEditorFrame::onMouseMoved);
+		ofRemoveListener(ev.mouseDragged, this, &ofxEditorFrame::onMouseDragged);
+		ofRemoveListener(ev.mouseScrolled, this, &ofxEditorFrame::onMouseScrolled);
+
+		is_enabled_mouse_interaction_ = false;
+	}
+}
+
+void ofxEditorFrame::pushMatrix() const
+{
+	ofPushMatrix();
+	ofTranslate(region_.position);
+	ofScale(scale_, scale_);
+	ofTranslate(-offset_);
+}
+void ofxEditorFrame::popMatrix() const
+{
+	ofPopMatrix();
+}
+void ofxEditorFrame::pushScissor() const
+{
+	scissor_cache_.is_enabled = glIsEnabled(GL_SCISSOR_TEST);
+	if(scissor_cache_.is_enabled) {
+		glGetIntegerv(GL_SCISSOR_BOX, scissor_cache_.box);
+	}
+	else {
+		glEnable(GL_SCISSOR_TEST);
+	}
+	glScissor(region_.getLeft(), ofGetHeight()-region_.getBottom(), region_.getWidth(), region_.getHeight());
+}
+void ofxEditorFrame::popScissor() const
+{
+	if(scissor_cache_.is_enabled) {
+		glScissor(scissor_cache_.box[0], scissor_cache_.box[1], scissor_cache_.box[2], scissor_cache_.box[3]);
+	}
+	else {
+		glDisable(GL_SCISSOR_TEST);
+	}
+}
+
+
+glm::vec2 ofxEditorFrame::getIn(const glm::vec2 &pos) const
+{
+	return (pos-glm::vec2(region_.position))/scale_ + offset_;
+}
+glm::vec2 ofxEditorFrame::getOut(const glm::vec2 &pos) const
+{
+	return (pos - offset_)*scale_ + glm::vec2(region_.position);
+}
+
+void ofxEditorFrame::translate(glm::vec2 on_screen_delta)
+{
+	offset_ -= on_screen_delta/scale_;
+}
+
+void ofxEditorFrame::scale(float mul_scale, glm::vec2 on_screen_anchor)
+{
+	float scale_prev = scale_;
+	scale_ *= mul_scale;
+	if(scale_ < settings_.min_scale) {
+		scale_ = settings_.min_scale;
+	}
+	offset_ -= (getIn(on_screen_anchor)-offset_)*(1-scale_/scale_prev);
+}
+
+void ofxEditorFrame::resetMatrix()
+{
+	offset_ = {0,0};
+	scale_ = 1;
+}
+
+void ofxEditorFrame::onMousePressed(ofMouseEventArgs &arg)
+{
+	mouse_prev_ = mouse_;
+	mouse_pos_pressed_ = 
+	mouse_.pos = arg;
+	mouse_.button_flag |= (1<<arg.button);
+	mouse_.scroll = {0,0};
+	has_unprocessed_mouse_event_ = true;
+}
+void ofxEditorFrame::onMouseReleased(ofMouseEventArgs &arg)
+{
+	mouse_.button_flag &= ~(1<<arg.button);
+	mouse_.scroll = {0,0};
+	has_unprocessed_mouse_event_ = true;
+}
+void ofxEditorFrame::onMouseMoved(ofMouseEventArgs &arg)
+{
+	mouse_.pos = arg;
+//	mouse_.button_flag = 0;
+	mouse_.scroll = {0,0};
+	has_unprocessed_mouse_event_ = true;
+}
+void ofxEditorFrame::onMouseDragged(ofMouseEventArgs &arg)
+{
+	mouse_.pos = arg;
+//	mouse_.button_flag |= (1<<arg.button);
+	mouse_.scroll = {0,0};
+	has_unprocessed_mouse_event_ = true;
+}
+void ofxEditorFrame::onMouseScrolled(ofMouseEventArgs &arg)
+{
+	mouse_.scroll.x = arg.scrollX;
+	mouse_.scroll.y = arg.scrollY;
+	has_unprocessed_mouse_event_ = true;
+}
---
+++ src/ofxEditorFrame.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include "ofRectangle.h"
+#include "ofEvents.h"
+#include <glm/vec2.hpp>
+#include <glm/glm.hpp>
+
+class ofxEditorFrame
+{
+public:
+	~ofxEditorFrame();
+	void setup();
+	void update();
+
+	void pushMatrix() const;
+	void popMatrix() const;
+	void pushScissor() const;
+	void popScissor() const;
+	
+	void enableMouseInteraction();
+	void disableMouseInteraction();
+	void toggleMouseInteractionEnabled() { isMouseInteractionEnabled() ? disableMouseInteraction() : enableMouseInteraction(); }
+	bool isMouseInteractionEnabled() const { return is_enabled_mouse_interaction_; }
+
+	void setRegion(const ofRectangle &region) { region_ = region; }
+	ofRectangle getRegion() const { return region_; }
+	
+	void translate(glm::vec2 on_screen_delta);
+	void scale(float mul_scale, glm::vec2 on_screen_anchor);
+	void resetMatrix();
+	
+	glm::vec2 getIn(const glm::vec2 &outer_pos) const;
+	glm::vec2 getOut(const glm::vec2 &inner_pos) const;
+	glm::vec2 getTranslate() const { return offset_; }
+	float getScale() const { return scale_; }
+
+	void onMouseEvent(ofMouseEventArgs &arg);
+	void onMousePressed(ofMouseEventArgs &arg);
+	void onMouseReleased(ofMouseEventArgs &arg);
+	void onMouseMoved(ofMouseEventArgs &arg);
+	void onMouseDragged(ofMouseEventArgs &arg);
+	void onMouseScrolled(ofMouseEventArgs &arg);
+
+	struct MouseEventArg {
+		glm::vec2 pos, delta, drag, scroll;
+		uint32_t pressed_flag, pressing_flag, released_flag;
+		float parent_scale;
+		ofRectangle getDragRect() const {
+			return {pos-drag, drag.x, drag.y};
+		}
+		bool isClicked(uint32_t button=OF_MOUSE_BUTTON_LEFT, float threshold=0) const {
+			return isReleased(button) && glm::length(drag)<=threshold;
+		}
+		bool isDragged(uint32_t button=OF_MOUSE_BUTTON_LEFT, float threshold=0) const {
+			return isPressing(button) && glm::length(delta)>=threshold;
+		}
+		bool isPressed(uint32_t button=OF_MOUSE_BUTTON_LEFT) const { return (pressed_flag&(1<<button)) != 0; }
+		bool isPressing(uint32_t button=OF_MOUSE_BUTTON_LEFT) const { return (pressing_flag&(1<<button)) != 0; }
+		bool isReleased(uint32_t button=OF_MOUSE_BUTTON_LEFT) const { return (released_flag&(1<<button)) != 0; }
+		bool isScrolledX(float threshold=0) const { return glm::abs(scroll.x) >= threshold; }
+		bool isScrolledY(float threshold=0) const { return glm::abs(scroll.y) >= threshold; }
+		bool isScrolled(float threshold=0) const { return glm::length(scroll) >= threshold; }
+	};
+	ofEvent<const MouseEventArg> on_mouse_event_;
+protected:
+	bool is_enabled_mouse_interaction_=false;
+	float scale_=1;
+	glm::vec2 offset_;
+	ofRectangle region_;
+	
+	struct Settings {
+		float min_scale = 0.1f;
+	} settings_;
+private:
+	struct ScissorCache {
+		bool is_enabled;
+		GLint box[4];
+	};
+	mutable ScissorCache scissor_cache_;
+	struct MouseArg {
+		glm::vec2 pos, scroll;
+		uint32_t button_flag;
+	};
+	glm::vec2 mouse_pos_pressed_;
+	bool has_unprocessed_mouse_event_=false;
+	bool is_mouse_event_sent_once_=false;
+	MouseArg mouse_, mouse_prev_;
+	MouseEventArg makeArg() {
+		return makeArg(mouse_, mouse_prev_, mouse_pos_pressed_);
+	}
+	MouseEventArg makeArg(const MouseArg &current, const MouseArg &prev, const glm::vec2 &pos_pressed) {
+		MouseEventArg ret;
+		ret.pos = current.pos;
+		ret.scroll = current.scroll;
+		ret.pressing_flag = current.button_flag;
+		if(is_mouse_event_sent_once_) {
+			ret.delta = ret.pos - prev.pos;
+			ret.drag = ret.pos - pos_pressed;
+			ret.pressed_flag = current.button_flag & ~prev.button_flag;
+			ret.released_flag = ~current.button_flag & prev.button_flag;
+		}
+		return ret;
+	}
+};
+
